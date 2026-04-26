1db8c85076ab666e050e17e8b6ef1f0beaed86a9
Thejas ks <thejasks2005@gmail.com>
2026-04-26 20:00:15 +0530

    fix: switch to PostgreSQL for Render deployment

---
+++ backend-mini-mindforge/src/main/java/com/thejas/backend_mini_mindforge/config/DataSourceConfig.java
@@ -0,0 +1,35 @@
+package com.thejas.backend_mini_mindforge.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String rawUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        String jdbcUrl = rawUrl.startsWith("postgresql://")
+                ? "jdbc:" + rawUrl
+                : rawUrl;
+
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(jdbcUrl);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName("org.postgresql.Driver");
+        return ds;
+    }
+}
